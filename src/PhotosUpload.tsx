f34b1354a10f141acab2f50cb1497fde9c94dc4d
kusumoto01 <74345611+kusumoto01@users.noreply.github.com>
2020-12-21 17:52:41 +0900

    2020/12/21

--- src/PhotosUpload.tsx
+++ src/PhotosUpload.tsx
@@ -1,7 +1,37 @@
 import React, { ChangeEvent, createRef, Fragment, useState } from 'react'
+import { css } from '@emotion/core'
+import Compress from 'react-image-file-resizer'
+// import imageCompression from 'browser-image-compression'
 
-const photosUpload: React.FC = () => {
-    const [src, setSrc] = useState('')
+type Props = {
+    imageURL: string
+}
+// declare function require(x: string): any
+// const webp = require('webp-converter')
+const Upload = css({
+    position: 'absolute',
+    top: '50px',
+    left: '5vw',
+    width: '40%',
+    height: '300px'
+})
+
+const UploadButton = css({
+    position: 'absolute',
+    left: '20vw',
+    top: '20px'
+})
+
+const PhotosActiveZone = css({
+    position: 'absolute',
+    left: '20vw',
+    top: '50px',
+    width: '30%',
+    height: '150px'
+})
+
+const PhotosUpload: React.FC<Props> = ({ imageURL }) => {
+    const [image, setImage] = useState(imageURL)
     const ref = createRef<HTMLInputElement>()
     const onClick = () => {
         if (ref.current) {
@@ -9,6 +39,11 @@ const photosUpload: React.FC = () => {
         }
     }
     const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+        // const options = {
+        //     maxSizeMB: 0.2,
+        //     maxWidthOrHeight: 50
+        // }
+
         if (event.target.files === null) {
             return
         }
@@ -16,25 +51,57 @@ const photosUpload: React.FC = () => {
         if (file === null) {
             return
         }
+        Compress.imageFileResizer(
+            file, // the file from input
+            100, // width
+            100, // height
+            'WEBP', // compress format WEBP, JPEG, PNG
+            50, // quality
+            0, // rotation
+            uri => {
+                console.log(uri)
+                // You upload logic goes here
+            },
+            'base64' // blob or base64 default base64
+        )
+        // const compressFile = imageCompression(file, options)
+        //     .then(function (compressedFile) {
+        //         console.log(
+        //             'compressedFile instanceof Blob',
+        //             compressedFile instanceof Blob
+        //         ) // true
+        //         console.log(
+        //             `compressedFile size ${
+        //                 compressedFile.size / 1024 / 1024
+        //             } MB`
+        //         ) // smaller than maxSizeMB
+        //     })
+        //     .catch(function (error) {
+        //         console.log(error.message)
+        //     })
+        // // webp.grant_permission()
         let reader = new FileReader()
         reader.readAsDataURL(file)
         reader.onload = () => {
-            setSrc(reader.result as string)
+            setImage(reader.result as string)
         }
     }
 
     return (
         <Fragment>
             <input
+                css={Upload}
                 onChange={onChange}
                 ref={ref}
                 style={{ display: 'none' }}
                 type={'file'}
             />
-            <button onClick={onClick}>file</button>
-            <div>{src && <img src={src} />}</div>
+            <button css={UploadButton} onClick={onClick}>
+                file
+            </button>
+            <div>{image && <img css={PhotosActiveZone} src={image} />}</div>
         </Fragment>
     )
 }
 
-export default photosUpload
+export default PhotosUpload
--- src/movelist.tsx
+++ src/movelist.tsx
@@ -1,4 +1,5 @@
 import React, { useRef, useState } from 'react'
+import { css } from '@emotion/core'
 
 type Props = {
     AdataLeft: {
@@ -21,105 +22,149 @@ type Props = {
         price: number
     }[]
 }
+const buttonStyleLeft = css({
+    position: 'relative',
+    width: 100,
+    fontSize: '1vw',
+    top: '20vw',
+    left: '33vw'
+})
+
+const buttonStyleRight = css({
+    position: 'relative',
+    width: 100,
+    fontSize: '1vw',
+    top: '20vw',
+    left: '48vw'
+})
+
+const MoveRightButton = css({
+    position: 'relative',
+    width: 100,
+    fontSize: '2vw',
+    top: '18vw',
+    left: '20vw'
+})
+
+const MoveLeftButton = css({
+    position: 'relative',
+    width: 100,
+    fontSize: '2vw',
+    top: '22vw',
+    left: '20vw'
+})
+const multipleLeft = css({
+    paddingRight: '1em',
+    cursor: 'pointer',
+    fontSize: '2vw',
+    textIndent: '0.01px',
+    textOverflow: 'ellipsis',
+    outline: 'none',
+    background: '#ffffff transparent',
+    backgroundImage: 'none',
+    boxShadow: 'none',
+    WebkitAppearance: 'none',
+    appearance: 'none',
+    position: 'relative',
+    border: '1px solid #bbbbbb',
+    borderRadius: '2px',
+    width: '400px',
+    height: '300px',
+    top: '20vw',
+    left: '40%'
+})
+
+const multipleRight = css({
+    paddingRight: '1em',
+    cursor: 'pointer',
+    fontSize: '2vw',
+    textIndent: '0.01px',
+    textOverflow: 'ellipsis',
+    outline: 'none',
+    background: '#ffffff transparent',
+    backgroundImage: 'none',
+    boxShadow: 'none',
+    WebkitAppearance: 'none',
+    appearance: 'none',
+    position: 'relative',
+    border: '1px solid #bbbbbb',
+    borderRadius: '2px',
+    width: '400px',
+    height: '300px',
+    top: '20vw',
+    left: '90%'
+})
 
 const Movelist: React.FC<Props> = ({ AdataLeft, AdataRight }) => {
     const [dataLeft, setDataLeft] = useState(AdataLeft)
-
     const [dataRight, setDataRight] = useState(AdataRight)
-
     const [selectLeft, setSelectLeft] = useState<number[]>()
-
     const [selectRight, setSelectRight] = useState<number[]>()
-
     const leftElm = useRef<HTMLSelectElement>(null)
-
     const rightElm = useRef<HTMLSelectElement>(null)
 
     // 左→右への移動
-
     const onMoveRight = (): void => {
         for (const i of selectLeft!) {
             const selectData = dataLeft.filter(list => list.id === Number(i))
-
             setDataLeft(dataLeft =>
                 dataLeft.filter(list => list.id !== Number(i))
             )
-
             setDataRight(dataRight => [...selectData, ...dataRight])
         }
-
         setDataRight(dataRight =>
             dataRight.sort((a, b) => a.sortkey - b.sortkey)
         )
     }
-
     // 右→左への移動
-
     const onMoveLeft = (): void => {
         for (const i of selectRight!) {
             const selectData = dataRight.filter(list => list.id === Number(i))
-
             setDataRight(dataRight =>
                 dataRight.filter(list => list.id !== Number(i))
             )
-
             setDataLeft(dataLeft => [...selectData, ...dataLeft])
         }
-
         setDataLeft(dataLeft => dataLeft.sort((a, b) => a.sortkey - b.sortkey))
     }
 
     // 要素選択時(左)
-
     const setSelectedLeft = (): void => {
         const valAry: number[] = []
-
         for (const boxLeft of Array.from(leftElm!.current!)) {
             if ((boxLeft as HTMLOptionElement).selected)
                 valAry.push(Number((boxLeft as HTMLOptionElement).value))
         }
-
         setSelectLeft(valAry)
     }
 
     //要素選択時(右)
-
     const setSelectedRight = (): void => {
         const valAry: number[] = []
-
         for (const boxRight of Array.from(rightElm!.current!)) {
             if ((boxRight as HTMLOptionElement).selected)
                 valAry.push(Number((boxRight as HTMLOptionElement).value))
         }
-
         setSelectRight(valAry)
     }
 
     // 全選択ボタン押下時(左)
-
     const setSelectedAllLeft = (): void => {
         const valAry: number[] = []
-
         for (const boxLeft of Array.from(leftElm!.current!)) {
             ;(boxLeft as HTMLOptionElement).selected = true
-
             valAry.push(Number((boxLeft as HTMLOptionElement).value))
         }
-
         setSelectLeft(valAry)
     }
 
     // 全選択ボタン押下時(右)
-
     const setSelectedAllRight = (): void => {
         const valAry: number[] = []
-
         for (const boxRight of Array.from(rightElm!.current!)) {
             ;(boxRight as HTMLOptionElement).selected = true
-
             valAry.push(Number((boxRight as HTMLOptionElement).value))
         }
-
         setSelectRight(valAry)
     }
 
@@ -129,19 +174,16 @@ const Movelist: React.FC<Props> = ({ AdataLeft, AdataRight }) => {
                 <tr>
                     <td>
                         <input
-                            className="button_square button3"
+                            css={buttonStyleLeft}
                             type="button"
                             id="selectAll_box_left"
                             value="全選択"
                             onClick={setSelectedAllLeft}
                         />
-
                         <br />
-
                         <br />
-
                         <select
-                            className="select_box"
+                            css={multipleLeft}
                             id="select_box_left"
                             name="s1"
                             size={5}
@@ -156,44 +198,36 @@ const Movelist: React.FC<Props> = ({ AdataLeft, AdataRight }) => {
                             ))}
                         </select>
                     </td>
-
                     <td>
                         <input
-                            className="button_square button1"
+                            css={MoveRightButton}
                             type="button"
                             name="right"
                             value="≫"
                             onClick={onMoveRight}
                         />
-
                         <br />
-
                         <br />
-
                         <input
-                            className="button_square button2"
+                            css={MoveLeftButton}
                             type="button"
                             name="left"
                             value="≪"
                             onClick={onMoveLeft}
                         />
                     </td>
-
                     <td>
                         <input
-                            className="button_square button4"
+                            css={buttonStyleRight}
                             type="button"
                             id="selectAll_box_right"
                             value="全選択"
                             onClick={setSelectedAllRight}
                         />
-
                         <br />
-
                         <br />
-
                         <select
-                            className="select_box"
+                            css={multipleRight}
                             id="select_box_right"
                             name="s2"
                             size={5}
@@ -213,5 +247,4 @@ const Movelist: React.FC<Props> = ({ AdataLeft, AdataRight }) => {
         </table>
     )
 }
-
 export default Movelist
